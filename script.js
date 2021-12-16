c49afb6edc310af3465a84ddae6cf72f543aaa9c
LeoKuD <then7w@gmail.com>
2021-12-16 15:34:19 +0300

    refactor: all expect em, rem

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.querySelectorAll('.gallery img').forEach((image) => {
-  
   image.onclick = () => {
     function hide(e) {
       if (e.target !== img) {
@@ -8,25 +7,20 @@ document.querySelectorAll('.gallery img').forEach((image) => {
         document.body.style.overflow = 'visible';
       }
     }
-    
+
     let img = document.querySelector('.popup img');
     let popup = document.querySelector('.popup');
     popup.style.display = 'block';
     let smileImg = image.getAttribute('src');
-    img.src = smileImg;
+    let bigImg = smileImg.slice(0, smileImg.indexOf('.web')) + 'big.webp';
+    img.src = bigImg;
+    console.log(bigImg);
     document.body.style.overflow = 'hidden';
     popup.addEventListener('click', hide);
   };
 });
 
-document.body.addEventListener(
-  "load",
-  (e) => {
-    if (e.target.tagName != "IMG") {
-      return;
-    }
-    // Remove the blurry placeholder.
-    e.target.style.backgroundImage = "none";
-  },
-  /* capture */ true
-);
+document.querySelector('.form').onsubmit = function submit(e) {
+  e.preventDefault();
+  document.querySelectorAll('input').forEach((input) => console.log(input.name, ': ', input.value))
+};
